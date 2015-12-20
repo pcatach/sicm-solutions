185d6320cbec1d7842fbac6746d01eb6c1c50a03
Patrick Eli Catach <patrick.catach@gmail.com>
2015-12-20 16:05:05 -0200

    corrected ex. number

--- chapter1/ex1-5.scm
+++ chapter1/ex1-5.scm
@@ -1,10 +1,12 @@
+; exercise 1-5. Solution Process
+
 (define ((L-free-particle mass) local)
   (let ((v (velocity local)))
     (* 1/2 mass (dot-product v v))))
 
 (define ((L-harmonic m k) local)
   (let ((q (coordinate local))
-	(v (velocity local)))
+        (v (velocity local)))
     (- (* 1/2 m (square v)) (* 1/2 k (square q)))))
 
 (define (Lagrangian-action L q t1 t2)
@@ -30,14 +32,9 @@
 (define (find-path Lagrangian t0 q0 t1 q1 n)
   (let ((initial-qs (linear-interpolants q0 q1 n)))
     (let ((minimizing-qs
-	   (multidimensional-minimize
-	    (parametric-path-action Lagrangian t0 q0 t1 q1)
-	    initial-qs)))
+           (multidimensional-minimize
+            (parametric-path-action Lagrangian t0 q0 t1 q1)
+            initial-qs)))
       (make-path t0 q0 t1 q1 minimizing-qs))))
 
-(find-path (L-harmonic 1.0 1.0) 0. 1. :pi/2 0   . 2) 
-
-
-
-
-
+(find-path (L-harmonic 1.0 1.0) 0. 1. :pi/2 0. 2)
